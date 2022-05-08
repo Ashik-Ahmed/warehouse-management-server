ee16f9eeec03431bb8294e98c13188faac2648c9
Ashik-Ahmed <ashikahmed121@gmail.com>
2022-05-08 23:37:37 +0600

    jwt implemented

--- index.js
+++ index.js
@@ -14,6 +14,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden Access' });
+        }
+        console.log('Decoded', decoded);
+        req.decoded = decoded;
+        next();
+    })
+}
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.m12jl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -62,14 +78,24 @@ async function run() {
         })
 
         // get products by user email 
-        app.get('/myproducts', async (req, res) => {
+        app.get('/myproducts', verifyJWT, async (req, res) => {
+
+            const authHeader = req.headers.authorization;
+            console.log(authHeader);
+
             // const decodedEmail = req.decoded.email;
             const email = req.query.email;
+            const decodedEmail = req.decoded.email;
 
-            const query = { email: email };
-            const cursor = productCollection.find(query);
-            const products = await cursor.toArray();
-            res.send(products);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = productCollection.find(query);
+                const products = await cursor.toArray();
+                res.send(products);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' })
+            }
 
         })
 
